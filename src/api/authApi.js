9894bcf72fba372cbd0ea05ae1c284c06af8ce02
Rachel <rachelibr050@gmail.com>
2022-12-19 16:27:05 +0200

    deleted unnecessary files

--- src/api/authApi.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient"
-
-const authApi = {
-  signup: params => {axiosClient.post('user/registration', {params})},
-  login: params => axiosClient.post('auth/authenticate', params),
-  //verifyToken: () => axiosClient.post('auth/verify-token')
-}
-
-export default authApi
